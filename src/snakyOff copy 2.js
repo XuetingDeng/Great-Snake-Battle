a59d7429320f1fca1f755cf99548a4e3c17ef576
SkyeKD <skye.kexin@gmail.com>
2023-11-17 16:58:48 -0800

    two local snakes

--- src/snakyOff copy 2.js
+++ src/snakyOff copy 2.js
@@ -6,10 +6,16 @@ let snakeDeath2 = false; //设置初始蛇存活状态
 let foodMultiple = []; // 生成food array
 const foodNum = 15;
 const foodColors = ['#b6dcb6', '#d2e9e1', '#fbedc9', '#f8dda9', '#fcb6d0', '#ffdee1', '#8ac6d1', '#d9d913'];//food color set
-let snakePlayer1 = {}; // 第一条蛇
-let snakePlayer2 = {}; // 第二条蛇
-
+let snakePlayer1 = createInitialSnake(); // 第一条蛇
+let snakePlayer2 = createInitialSnake(); // 第二条蛇
+let dxPlayer1 = gridSize; // 第一条蛇的移动方向
+let dyPlayer1 = 0;
+let dxPlayer2 = gridSize; // 第二条蛇的移动方向
+let dyPlayer2 = 0;
+let snakeSpeed1 = 500; // 初始蛇的移动速度
+let snakeSpeed2 = 500; // 初始蛇的移动速度
 // 蛇的初始位置随机
+// let snake = createInitialSnake();
 function createInitialSnake() {
   const startPosition = createRandomPosition();
   return [
@@ -17,27 +23,7 @@ function createInitialSnake() {
     { x: startPosition.x - gridSize, y: startPosition.y },
   ];
 }
-function initializeSnakes() {
-  snakePlayer1 = {
-    id:1,
-    body: createInitialSnake(),
-    dx: gridSize,
-    dy: 0,
-    speed: 500,
-    isDead: false
-  };
 
-  snakePlayer2 = {
-    id:2,
-    body: createInitialSnake(),
-    dx: gridSize,
-    dy: 0,
-    speed: 500,
-    isDead: false
-  };
-
-  snakes = [snakePlayer1, snakePlayer2];
-}
 function createRandomPosition() {
   const maxX = (canvas.width / gridSize) - 10;
   const maxY = (canvas.height / gridSize) - 5;
@@ -96,9 +82,7 @@ function drawSnakePart(snakePart, idx) {
 //retrieve蛇蛇组成一条大蛇
 //后续增加对应的蛇头
 function drawSnake(snake) {
-  if(!snake.isDead){
-    snake.body.forEach((part, idx) => drawSnakePart(part, idx));
-  }
+  snake.forEach((part, idx) => drawSnakePart(part, idx));
 }
 
 function drawFood() {
@@ -111,12 +95,12 @@ function drawFood() {
 }
 
 //控制蛇蛇的移动
-function advanceSnake(snake) {
-  if (snake.isDead) {
+function advanceSnake(snake, dx, dy, snakeDeath) {
+  if (snakeDeath) {
     return; //死了就结束游戏
   }
-  const head = { x: snake.body[0].x + snake.dx, y: snake.body[0].y + snake.dy };
-  snake.body.unshift(head); //inserts the given values to the beginning of an array-like object
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head); //inserts the given values to the beginning of an array-like object
 
   //check head位置是否与food一样
   let didEatFood = foodMultiple.findIndex(food => food.x === head.x && food.y === head.y);
@@ -127,10 +111,10 @@ function advanceSnake(snake) {
     foodMultiple.push(createRandomFood());
   } else {
     //没吃到food, pop出snake[]最后一个元素
-    snake.body.pop();
+    snake.pop();
   }
 
-  checkDeath();
+  checkDeath(snake);
 }
 
 function clearCanvas() {
@@ -151,136 +135,114 @@ function changeDirection(event) {
   const DOWN_KEY = 40;
 
   const keyPressed = event.keyCode;
-  const goingUpPlayer2 = snakePlayer2.dy === -gridSize;
-  const goingDownPlayer2 = snakePlayer2.dy === gridSize;
-  const goingRightPlayer2 = snakePlayer2.dx === gridSize;
-  const goingLeftPlayer2 = snakePlayer2.dx === -gridSize;
+  const goingUpPlayer2 = dyPlayer2 === -gridSize;
+  const goingDownPlayer2 = dyPlayer2 === gridSize;
+  const goingRightPlayer2 = dxPlayer2 === gridSize;
+  const goingLeftPlayer2 = dxPlayer2 === -gridSize;
 
-  const goingUpPlayer1 = snakePlayer1.dy === -gridSize;
-  const goingDownPlayer1 = snakePlayer1.dy === gridSize;
-  const goingRightPlayer1 = snakePlayer1.dx === gridSize;
-  const goingLeftPlayer1 = snakePlayer1.dx === -gridSize;
+  const goingUpPlayer1 = dyPlayer1 === -gridSize;
+  const goingDownPlayer1 = dyPlayer1 === gridSize;
+  const goingRightPlayer1 = dxPlayer1 === gridSize;
+  const goingLeftPlayer1 = dxPlayer1 === -gridSize;
   // 控制第一条蛇
   if (keyPressed === W_KEY && !goingDownPlayer1) {
-    snakePlayer1.dx = 0;
-    snakePlayer1.dy = -gridSize;
+    dxPlayer1 = 0;
+    dyPlayer1 = -gridSize;
   } else if (keyPressed === A_KEY && !goingRightPlayer1) {
-    snakePlayer1.dx = -gridSize;
-    snakePlayer1.dy = 0;
+    dxPlayer1 = -gridSize;
+    dyPlayer1 = 0;
   } else if (keyPressed === S_KEY && !goingUpPlayer1) {
-    snakePlayer1.dx = 0;
-    snakePlayer1.dy = gridSize;
+    dxPlayer1 = 0;
+    dyPlayer1 = gridSize;
   } else if (keyPressed === D_KEY && !goingLeftPlayer1) {
-    snakePlayer1.dx = gridSize;
-    snakePlayer1.dy = 0;
+    dxPlayer1 = gridSize;
+    dyPlayer1 = 0;
   }
   //按左键，只要snake不是向右运动的，就方向往左改变
   if (keyPressed === LEFT_KEY && !goingRightPlayer2) {
-    snakePlayer2.dx = -gridSize;
-    snakePlayer2.dy = 0;
+    dxPlayer2 = -gridSize;
+    dyPlayer2 = 0;
   }
   if (keyPressed === UP_KEY && !goingDownPlayer2) {
-    snakePlayer2.dx = 0;
-    snakePlayer2.dy = -gridSize;
+    dxPlayer2 = 0;
+    dyPlayer2 = -gridSize;
   }
   if (keyPressed === RIGHT_KEY && !goingLeftPlayer2) {
-    snakePlayer2.dx = gridSize;
-    snakePlayer2.dy = 0;
+    dxPlayer2 = gridSize;
+    dyPlayer2 = 0;
   }
   if (keyPressed === DOWN_KEY && !goingUpPlayer2) {
-    snakePlayer2.dx = 0;
-    snakePlayer2.dy = gridSize;
+    dxPlayer2 = 0;
+    dyPlayer2 = gridSize;
   }
 }
 
-function checkDeath() {
-  for(let snake of snakes){
-    const head = snake.body[0];
-    //if snake's head is out of bound, die
-    if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) {
-      markSnakeDead(snake);
-    }
-    checkSnakeCollision(snake);
+function checkDeath(snake) {
+  const head = snake[0];
+  //if snake's head is out of bound, die
+  if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) {
+    if (snake == snakePlayer1) { snakeDeath1 = true }else if(snake==snakePlayer2){snakeDeath2 = true}
+    // snakeDeath = true; //蛇死了
   }
-
+  console.log("checkDeath ", snake)
+  console.log("checkDeath1 ", snakePlayer1, snakeDeath1)
+  console.log("checkDeath2 ", snakePlayer2, snakeDeath2)
 }
- // 检查蛇头是否与其他蛇的身体相撞
-function checkSnakeCollision(snake){
-  for (let otherSnake of snakes) {
-    if (otherSnake !== snake) {
-      const head = snake.body[0];
-      for (let part of otherSnake.body) {
-        if (head.x === part.x && head.y === part.y) {
-          markSnakeDead(snake);
-          return;
-        }
-      }
-    }
-  }
-}
-//标记蛇的死亡，在snakes中删去这个蛇，并转化为食物
-function markSnakeDead(snake){
-  snake.isDead = true;
-  const index = snakes.indexOf(snake);
-  if (index !== -1) {
-    snakes.splice(index, 1);
-  }
-  for(let part of snake.body){
-    const food ={x:part.x,y:part.y,color:'#b6dcb6'}
-    foodMultiple.push(food)
-  }
-}
-
 
 initializeFood();
-initializeSnakes();
+
 function main() {
 
-  if (snakePlayer1.isDead && snakePlayer2.isDead) {
+  if (snakeDeath1 && snakeDeath2) {
     alert("Game Over Snake1&2 dead");
     return;
   }
+  // if (snakeDeath2) {
+  //   alert("Game Over Snake2 dead");
+  //   return;
+  // }
 
   setTimeout(function onTick() {
     clearCanvas();
     // initializeFood(); to do... 后续可以改成随着固定的时间额外增加一丢丢food
     drawFood();
-    for(let snake of snakes){
-      advanceSnake(snake);
-      drawSnake(snake);
-    }
+    // advanceSnake();
+    // drawSnake();
+    advanceSnake(snakePlayer1, dxPlayer1, dyPlayer1, snakeDeath1);
+    drawSnake(snakePlayer1);
+    advanceSnake(snakePlayer2, dxPlayer2, dyPlayer2, snakeDeath2);
+    drawSnake(snakePlayer2);
     main();
-  }, Math.min(300));//延迟150ms
-  
+  }, Math.min(snakeSpeed1, snakeSpeed2));//延迟150ms
 
 }
 
 //键盘按下事件监听器
 document.addEventListener('keydown', changeDirection);
-// document.addEventListener('keydown', function (event) {
-//   if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
-//     // 如果按下的是右Shift键
-//     snakeSpeed2 = 200; // 或者你想要的其他速度
-//   }
-// });
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+    // 如果按下的是右Shift键
+    snakeSpeed2 = 200; // 或者你想要的其他速度
+  }
+});
 
-// document.addEventListener('keyup', function (event) {
-//   if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
-//     // 如果释放的是右Shift键
-//     snakeSpeed2 =500; // 恢复初始速度
-//   }
-// });
-// document.addEventListener('keydown', function (event) {
-//   if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
-//     // 如果按下的是左Shift键
-//     snakeSpeed1 = 200; // 或者你想要的其他速度
-//   }
-// });
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+    // 如果释放的是右Shift键
+    snakeSpeed2 =500; // 恢复初始速度
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
+    // 如果按下的是左Shift键
+    snakeSpeed1 = 200; // 或者你想要的其他速度
+  }
+});
 
-// document.addEventListener('keyup', function (event) {
-//   if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
-//     // 如果释放的是左Shift键
-//     snakeSpeed1 = 500; // 恢复初始速度
-//   }
-// });
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Shift' && event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
+    // 如果释放的是左Shift键
+    snakeSpeed1 = 500; // 恢复初始速度
+  }
+});
 main();
